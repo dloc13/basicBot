83c1392c46841c12c40da95eb510a7725b0a3e49
DaffyDrone <simon.kerbusch@gmail.com>
2015-02-17 20:12:53 +0100

    test

--- basicBot.js
+++ basicBot.js
@@ -919,8 +919,8 @@
 								},
 						 
 								display: function (results) {
-									var country = results.getElementsByTagName("area")["name"].nodeValue;
-									var year = results.getElementsByTagName("begin");
+									var country = results.query.results.metadata["artist-list"].artist.area["name"];
+									var year = results.query.results.metadata["artist-list"].artist.["life-span"]["begin"];
 									console.log(country + " " + year);
 								}
 						}
